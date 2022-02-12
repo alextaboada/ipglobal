96b02baee140e75db2940cdc494960c99af904ae
Alejandro Taboada <alejandro.taboada.galdo@gmail.com>
2022-02-12 12:58:34 +0100

    FIX:arreglos en use

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use app\Models\Post;
+use App\Models\Post;
 
 class PostController extends Controller
 {
