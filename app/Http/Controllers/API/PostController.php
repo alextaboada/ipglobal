499eb01d6479fe3fc60c57069aeb49990f086576
Alejandro Taboada <alejandro.taboada.galdo@gmail.com>
2022-02-12 12:03:17 +0100

    ADD:ruta y metodo para crear post por API

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -12,4 +12,13 @@ class PostController extends Controller
     {
         return response(Post::find($id),200);
     }
+
+    public function storePost(Request $request){
+        $post=Post::create([
+            'title' => $request->title,
+            'body' => $request->body,
+            'author_id' => $request->author_id,
+        ]);
+        return response($post,201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PostController;
 
 Route::get('/post/{id}', [PostController::class, 'getPost'])->name('api.show');
+Route::post('/post', [PostController::class,'StorePost'])->name('api.create');
