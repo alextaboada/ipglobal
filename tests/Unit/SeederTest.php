23bf8431edbda106fd39a9298a31bd9adaf9e0ae
Alejandro Taboada <alejandro.taboada.galdo@gmail.com>
2022-02-12 12:34:34 +0100

    ADD:test unitario cargar datos en base de datos

---
+++ tests/Unit/SeederTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use Database\Seeders\authorsSeeder;
+use Database\Seeders\postsSeeder;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class SeederTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function seederCargaDatosAutoresCorrectamente()
+    {
+        $this->seed(authorsSeeder::class);
+
+        $this->assertDatabaseCount('authors',10);
+
+    }
+    /** @test */
+    public function seederCargaDatosPostsCorrectamente()
+    {
+        $this->seed([authorsSeeder::class,postsSeeder::class]);
+
+        $this->assertDatabaseCount('posts',100);
+
+    }
+}
